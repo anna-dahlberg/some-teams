453ef2133055edac2a8bd4b85b24ba411f486f46
anna-dahlberg <anna.dahlberg@live.se>
2024-10-08 12:11:59 +0200

    updated foreach

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ require __DIR__ . "/data.php";
 <article class="some-teams-article">
     <?php foreach ($teams as $teamName => $teamData): ?>
         <div class="team-div">
-            <img src="<?= $teamData['logo'] ?>" alt="The teams official logo" class="team-logo">
+            <img src="<?= $teamData['logo'] ?>" alt="Official logo of <?= $teamName ?>" class="team-logo">
             <h3 id="team-name"> <?= $teamName ?> </h3>
             League: <?= $teamData['league'] ?> <br>
             City: <?= $teamData['city'] ?> <br>
             UEFA Coefficient Ranking: <?= $teamData['uefa-coefficient-ranking'] ?> <br>
-            Group:<?= $teamData['group'] ?> <br>
-            <a href="<?= $teamData['url'] ?>">Official Website</a>
+            Group: <?= $teamData['group'] ?> <br>
+            <a href="<?= $teamData['url'] ?>" target="_blank">Official Website</a>
         </div>
     <?php endforeach; ?>
 </article>
